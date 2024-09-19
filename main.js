7d171bdcdaa17c220f57cadc150655c0b3038a8e
ExtraPolite <imextrapolite2@gmail.com>
2024-09-19 18:09:41 -0400

    make text delete before showing next

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import './style.css';
-import { gsap } from 'gsap';
-import { TextPlugin } from 'gsap/TextPlugin';
-gsap.registerPlugin(TextPlugin);
+import { animateTitle } from './animations';
 
-gsap.to('h1', { duration: 3, text: 'Journey Through the Solar System' });
+animateTitle();
